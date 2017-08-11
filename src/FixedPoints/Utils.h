6d6ceed966eeb4a6f2d33945ee5e79984f61a244
Pharap <pharap@hotmail.com>
2017-08-11 17:26:20 +0100

    Fixed casing in function implementations

--- src/FixedPoints/Utils.h
+++ src/FixedPoints/Utils.h
@@ -7,6 +7,11 @@
 // Declaration
 //
 
+//
+// Thanks to Arduino defining 'floor', 'ceil', 'round' etc as macros,
+// these functions cannot be converted to camel case and must remain as pascal case.
+//
+
 BEGIN_FIXED_POINTS
 template< unsigned Integer, unsigned Fraction >
 constexpr UFixedPoint<Integer, Fraction> Floor(const UFixedPoint<Integer, Fraction> & value);
@@ -40,7 +45,7 @@ constexpr UFixedPoint<Integer, Fraction> Floor(const UFixedPoint<Integer, Fracti
 {
 	using OutputType = UFixedPoint<Integer, Fraction>;
 	using InternalType = typename OutputType::InternalType;
-	return OutputType::FromInternal(static_cast<InternalType>(value.GetInternal() & ~Details::IdentityMask<Fraction>::Value));
+	return OutputType::fromInternal(static_cast<InternalType>(value.getInternal() & ~Details::IdentityMask<Fraction>::Value));
 }
 
 template< unsigned Integer, unsigned Fraction >
@@ -48,7 +53,7 @@ constexpr FixedPoint<Integer, Fraction> Floor(const FixedPoint<Integer, Fraction
 {
 	using OutputType = FixedPoint<Integer, Fraction>;
 	using InternalType = typename OutputType::InternalType;
-	return OutputType::FromInternal(static_cast<InternalType>(value.GetInternal() & ~Details::IdentityMask<Fraction>::Value));
+	return OutputType::fromInternal(static_cast<InternalType>(value.getInternal() & ~Details::IdentityMask<Fraction>::Value));
 }
 
 template< unsigned Integer, unsigned Fraction >
@@ -56,7 +61,7 @@ constexpr UFixedPoint<Integer, Fraction> Ceil(const UFixedPoint<Integer, Fractio
 {
 	using OutputType = UFixedPoint<Integer, Fraction>;
 	using InternalType = typename OutputType::InternalType;
-	return OutputType::FromInternal(static_cast<InternalType>((value.GetInternal() & ~Details::IdentityMask<Fraction>::Value) + (1 << Fraction)));
+	return OutputType::fromInternal(static_cast<InternalType>((value.getInternal() & ~Details::IdentityMask<Fraction>::Value) + (1 << Fraction)));
 }
 
 template< unsigned Integer, unsigned Fraction >
@@ -64,13 +69,13 @@ constexpr FixedPoint<Integer, Fraction> Ceil(const FixedPoint<Integer, Fraction>
 {
 	using OutputType = FixedPoint<Integer, Fraction>;
 	using InternalType = typename OutputType::InternalType;
-	return OutputType::FromInternal(static_cast<InternalType>((value.GetInternal() & ~Details::IdentityMask<Fraction>::Value) + (1 << Fraction)));
+	return OutputType::fromInternal(static_cast<InternalType>((value.getInternal() & ~Details::IdentityMask<Fraction>::Value) + (1 << Fraction)));
 }
 
 template< unsigned Integer, unsigned Fraction >
 constexpr UFixedPoint<Integer, Fraction> Round(const UFixedPoint<Integer, Fraction> & value)
 {
-	return (value.GetInternal() & Details::MsbMask<Fraction>::Value) != 0 ? Ceil(value) : Floor(value);
+	return (value.getInternal() & Details::MsbMask<Fraction>::Value) != 0 ? Ceil(value) : Floor(value);
 }
 
 template< unsigned Integer, unsigned Fraction >
@@ -78,8 +83,8 @@ constexpr FixedPoint<Integer, Fraction> Round(const FixedPoint<Integer, Fraction
 {
 	return
 		(
-			(value.GetInternal() & Details::MsbMask<Fraction>::Value) != 0 &&
-			(Signbit(value) ? (value.GetInternal() & Details::IdentityMask<Fraction - 1>::Value) != 0 : true)
+			(value.getInternal() & Details::MsbMask<Fraction>::Value) != 0 &&
+			(Signbit(value) ? (value.getInternal() & Details::IdentityMask<Fraction - 1>::Value) != 0 : true)
 		)
 		? Ceil(value)
 		: Floor(value);
@@ -88,6 +93,6 @@ constexpr FixedPoint<Integer, Fraction> Round(const FixedPoint<Integer, Fraction
 template< unsigned Integer, unsigned Fraction >
 constexpr bool Signbit(const FixedPoint<Integer, Fraction> & value)
 {
-	return (value.GetInternal() & Details::MsbMask<FixedPoint<Integer, Fraction>::InternalSize>::Value) != 0;
+	return (value.getInternal() & Details::MsbMask<FixedPoint<Integer, Fraction>::InternalSize>::Value) != 0;
 }
 END_FIXED_POINTS
