657c2136b8cf6fa408c5807464bd811ae1a0259d
Pharap <pharap@hotmail.com>
2017-08-26 23:57:14 +0100

    Rename common types to UQ format

--- src/FixedPointsCommon/UFixedPointCommon.h
+++ src/FixedPointsCommon/UFixedPointCommon.h
@@ -3,13 +3,13 @@
 #include "../FixedPoints.h" 
  
 BEGIN_FIXED_POINTS
-using Q4x4 = UFixedPoint<4, 4>;
-using Q8x8 = UFixedPoint<8, 8>;
-using Q16x16 = UFixedPoint<16, 16>;
-using Q32x32 = UFixedPoint<32, 32>;
+using UQ4x4 = UFixedPoint<4, 4>;
+using UQ8x8 = UFixedPoint<8, 8>;
+using UQ16x16 = UFixedPoint<16, 16>;
+using UQ32x32 = UFixedPoint<32, 32>;
 
-using Q1x7 = UFixedPoint<1, 7>;
-using Q1x15 = UFixedPoint<1, 15>;
-using Q1x31 = UFixedPoint<1, 31>;
-using Q1x63 = UFixedPoint<1, 63>;
+using UQ1x7 = UFixedPoint<1, 7>;
+using UQ1x15 = UFixedPoint<1, 15>;
+using UQ1x31 = UFixedPoint<1, 31>;
+using UQ1x63 = UFixedPoint<1, 63>;
 END_FIXED_POINTS
